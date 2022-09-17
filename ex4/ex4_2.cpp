72bb9df1e9142059f2c9daa3d731fc575271d704
openhe <625529334@qq.com>
2022-09-17 18:20:46 +0800

    9-17-ex4-2

--- ex4/ex4_2.cpp
+++ ex4/ex4_2.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
 #include <cmath>
+#include <utility>
 #include<opencv2/opencv.hpp>
 
-#define DURATION 500
-#define MIN_AREA 100
-#define MAX_AREA 400
-#define MAX_CIRCUMSTANCE 400
-#define MIN_CIRCUMSTANCE 50
+#define DURATION 100
+#define MIN_AREA1 90
+#define MAX_AREA1 900
+#define MAX_CIRCUMSTANCE1 800
+#define MIN_CIRCUMSTANCE1 50
+#define MAX_DISTANCE1 110
+
+#define MIN_AREA2 600
+#define MAX_AREA2 1200
+#define MAX_CIRCUMSTANCE2 400
+#define MIN_CIRCUMSTANCE2 50
+#define MAX_DISTANCE2 220
+#define MIN_DISTANCE2 150
+
+#define MAX_DELTA_HEIGHT 50
+
+#define Segment 67
 
 struct Point_Data {
     cv::Point center_point;
@@ -16,7 +29,7 @@ struct Point_Data {
     int y_max;
 };
 
-cv::Mat &recognize(cv::Mat src);
+void recognize(cv::Mat &src);
 
 double calc_threshold(const std::vector<std::vector<cv::Point>> &contours);
 
@@ -32,32 +45,44 @@ std::vector<Point_Data> desc_contours(const std::vector<std::vector<cv::Point>> 
 
 double dist(cv::Point p1, cv::Point p2);
 
-std::vector<std::pair<cv::Point, cv::Point>> filter(std::vector<Point_Data> point_data);
+std::vector<std::vector<cv::Point>> filter(std::vector<Point_Data> point_data);
+
+bool check_delta_height(Point_Data pd1,Point_Data pd2);
+
+int cnt=1;
+long frame_num;
 
 int main() {
     cv::VideoCapture capture(R"(D:\program\cpp\rm-test\ex4\res\d.mp4)");
-    long frame_num = capture.get(cv::CAP_PROP_FRAME_COUNT);
+    frame_num = capture.get(cv::CAP_PROP_FRAME_COUNT);
+    long frame_width=capture.get(cv::CAP_PROP_FRAME_WIDTH);
+    long frame_height=capture.get(cv::CAP_PROP_FRAME_HEIGHT);
+    cv::Size size=cv::Size(frame_width,frame_height);
     double fps = capture.get(cv::CAP_PROP_FPS);
+    cv::VideoWriter output=cv::VideoWriter(R"(D:\program\cpp\rm-test\ex4\output\d_res.avi)",
+                                           cv::VideoWriter::fourcc('P','I','M','1'),
+                                           fps,
+                                           size);
 
     cv::Mat frame;
-    capture >> frame;
-    cv::Mat res = recognize(frame);
-//    while (true){
-//        capture>>frame;
-//        if (frame.empty()) break;
-//        recognize(frame);
-//        imshow("origin-frame",frame);
-////        imshow("res",res);
-//        cv::waitKey(DURATION);
-//    }
 
+    while (true){
+        capture>>frame;
+        if (frame.empty()) break;
+//        imshow("origin-frame",frame);
+        recognize(frame);
+        imshow("recognized-frame",frame);
+//        cv::waitKey(DURATION);
+        cnt++;
+        output<<frame;
+    }
+
+    output.release();
     capture.release();
     return 0;
 }
 
-cv::Mat &recognize(cv::Mat src) {
-    cv::imshow("src", src);
-
+void recognize(cv::Mat &src) {
     //1. graying
     cv::Mat grey;
     cv::cvtColor(src, grey, cv::COLOR_BGR2GRAY);
@@ -78,6 +103,13 @@ cv::Mat &recognize(cv::Mat src) {
     std::vector<cv::Vec4i> hierachy;
     cv::findContours(canny_result, contours, hierachy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
 
+//    std::cout<<"Area:"<<std::endl;
+//    print_all_counter_area(contours);
+//    std::cout<<std::endl;
+//    std::cout<<"Circumstance:"<<std::endl;
+//    print_all_counter_circumstance(contours);
+//    std::cout<<std::endl;
+
 
     //5. remove the disturbance
     std::vector<std::vector<cv::Point>> res;
@@ -90,60 +122,50 @@ cv::Mat &recognize(cv::Mat src) {
     //6. draw the contour
     cv::Mat drawer = cv::Mat::zeros(cv::Size(src.cols, src.rows), CV_8UC3);
     cv::drawContours(drawer, res, -1, {255, 255, 255}, 3);
-    cv::imshow("contours", drawer);
+    cv::imshow("contours",drawer);
 
-    auto desc= desc_contours(contours);
-    filter(desc);
-
-
-//
-//    //7. draw the rectangle
-//    int x1 = 1e4, x2 = -1, y1 = 1e4, y2 = -1; // x1=min x,x2= max x,y1=min y,y2=max y
-//    for (auto sub: res) {
-//        for (auto point: sub) {
-//            x1 = std::min(x1, point.x);
-//            x2 = std::max(x2, point.x);
-//            y1 = std::min(y1, point.y);
-//            y2 = std::max(y2, point.y);
-//        }
-//    }
-//    cv::Point p1(x1, y1);
-//    cv::Point p2(x2, y1);
-//    cv::Point p3(x1, y2);
-//    cv::Point p4(x2, y2);
-//    cv::rectangle(src, p1, p4, cv::Scalar(255, 255, 0));
-//    cv::rectangle(drawer, p1, p4, cv::Scalar(255, 255, 0));
-//
-//    //8. draw the text
-//    cv::putText(src, "TARGETING AREA:", {5, 20}, cv::FONT_HERSHEY_TRIPLEX, 0.8, {255, 0, 255}, 1, false);
-//    cv::putText(src, point_to_str(p1, "p1"), {10, 50}, cv::FONT_HERSHEY_TRIPLEX, 0.6, {0, 255, 255}, 1, false);
-//    cv::putText(src, point_to_str(p2, "p2"), {10, 80}, cv::FONT_HERSHEY_TRIPLEX, 0.6, {0, 255, 255}, 1, false);
-//    cv::putText(src, point_to_str(p3, "p3"), {10, 110}, cv::FONT_HERSHEY_TRIPLEX, 0.6, {0, 255, 255}, 1, false);
-//    cv::putText(src, point_to_str(p4, "p4"), {10, 140}, cv::FONT_HERSHEY_TRIPLEX, 0.6, {0, 255, 255}, 1, false);
-//
-//    cv::imshow("src", src);
-////    cv::imwrite(R"(D:\program\cpp\rm-test\ex4\output\result.jpg)",src);
-//    cv::imshow("contours", drawer);
-////    cv::imwrite(R"(D:\program\cpp\rm-test\ex4\output\contours.jpg)",drawer);
-//    std::cout<<R"(you can find the final output at :\ex4\output\contours.jpg)"<<std::endl;
-    cv::waitKey(0);
+    //7. draw rectangle
+    std::vector<Point_Data> desc= desc_contours(res);
+    std::vector<std::vector<cv::Point>> targets=filter(desc);
+    for (const auto &target: targets){
+        cv::rectangle(drawer,target[0],target[3],cv::Scalar(255,255,0));
+        cv::rectangle(src,target[0],target[3],cv::Scalar(255,255,0));
+    }
 }
 
 bool judge_counter(const std::vector<cv::Point> &contour) {
-    return cv::contourArea(contour) >= MIN_AREA &&
-           cv::contourArea(contour) <= MAX_AREA &&
-           cv::arcLength(contour, true) <= MAX_CIRCUMSTANCE &&
-           cv::arcLength(contour, true) >= MIN_CIRCUMSTANCE;
+    if (cnt<=Segment){
+        return cv::contourArea(contour) >= MIN_AREA1 &&
+               cv::contourArea(contour) <= MAX_AREA1 &&
+               cv::arcLength(contour, true) <= MAX_CIRCUMSTANCE1 &&
+               cv::arcLength(contour, true) >= MIN_CIRCUMSTANCE1;
+    }else{
+        return cv::contourArea(contour) >= MIN_AREA2 &&
+               cv::contourArea(contour) <= MAX_AREA2 &&
+               cv::arcLength(contour, true) <= MAX_CIRCUMSTANCE2 &&
+               cv::arcLength(contour, true) >= MIN_CIRCUMSTANCE2;
+    }
+
 }
 
-std::vector<std::pair<cv::Point, cv::Point>> filter(std::vector<Point_Data> point_data) {
-    std::vector<std::pair<cv::Point, cv::Point>> res;
+std::vector<std::vector<cv::Point>> filter(std::vector<Point_Data> point_data) {
+    std::vector<std::vector<cv::Point>> res;
     for (int i = 0; i < point_data.size(); ++i) {
         for (int j = i+1; j < point_data.size(); ++j) {
             double dis=dist(point_data[i].center_point,point_data[j].center_point);
-            std::cout<<dis<<std::endl;
+            if ((cnt<=Segment&&dis<=MAX_DISTANCE1)||(cnt>Segment&&dis<=MAX_DISTANCE2&&dis>=MIN_DISTANCE2)) {
+                if (check_delta_height(point_data[i],point_data[j])){
+                    int x1=std::min(point_data[i].x_min,point_data[j].x_min);
+                    int x2=std::max(point_data[i].x_max,point_data[j].x_max);
+                    int y1=std::min(point_data[i].y_min,point_data[j].y_min);
+                    int y2=std::max(point_data[i].y_max,point_data[j].y_max);
+                    std::vector<cv::Point> tar={{x1,y1},{x2,y1},{x1,y2},{x2,y2}};
+                    res.push_back(tar);
+                }
+            }
         }
     }
+    std::cout<<std::endl;
     return res;
 }
 
@@ -161,6 +183,7 @@ std::vector<Point_Data> desc_contours(const std::vector<std::vector<cv::Point>> 
         y0=(y1+y2)>>1;
         point_data.push_back({{x0,y0},x1,x2,y1,y2});
     }
+    return point_data;
 }
 
 double dist(cv::Point p1, cv::Point p2) {
@@ -187,4 +210,9 @@ void print_all_counter_circumstance(const std::vector<std::vector<cv::Point>> &c
         std::cout << circumstances.top() << std::endl;
         circumstances.pop();
     }
+}
+
+bool check_delta_height(Point_Data pd1,Point_Data pd2){
+    return std::max(abs(pd1.y_min-pd2.y_min),
+             abs(pd1.y_max-pd2.y_max))<=MAX_DELTA_HEIGHT;
 }
